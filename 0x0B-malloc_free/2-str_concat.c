2045c805eee8fd4e4bf35ed2ce2125c58962a866
zahiiir <simo222zahir@gmail.com>
2023-10-31 12:01:42 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,43 +2,39 @@
 #include <stdlib.h>
 
 /**
-* str_concat - concatène deux chaînes de caractères
-* @s1: première chaîne
-* @s2: deuxième chaîne
-* Return: pointeur vers la nouvelle chaîne de caractères concaténée
-*/
-char *str_concat(char *s1, char *s2)
-{
-    char *conct;
-    int i, ci;
+ * str_concat - concatenates two strings together
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to the concatenated string, or NULL if it fails
+ */
+char *str_concat(char *s1, char *s2) {
+  size_t s1_len, s2_len, total_len;
+  char *str;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+  if (s1 == NULL) {
+    s1 = "";
+  }
+  if (s2 == NULL) {
+    s2 = "";
+  }
 
-    i = ci = 0;
-    while (s1[i] != '\0')
-        i++;
-    while (s2[ci] != '\0')
-        ci++;
+  // Calculate the total length of the concatenated string.
+  s1_len = strlen(s1);
+  s2_len = strlen(s2);
+  total_len = s1_len + s2_len + 1; // add 1 for the terminating null character
 
-    conct = malloc(sizeof(char) * (i + ci + 1));
-    if (conct == NULL)
-        return (NULL);
+  // Allocate memory for the concatenated string.
+  str = malloc(sizeof(char) * total_len);
+  if (str == NULL) {
+    return NULL;
+  }
 
-    i = ci = 0;
-    while (s1[i] != '\0')
-    {
-        conct[i] = s1[i];
-        i++;
-    }
-    while (s2[ci] != '\0')
-    {
-        conct[i] = s2[ci];
-        i++;
-        ci++;
-    }
-    conct[i] = '\0';
-    return (conct);
+  // Concatenate the two strings.
+  memcpy(str, s1, s1_len);
+  memcpy(str + s1_len, s2, s2_len);
+
+  // Add the terminating null character.
+  str[total_len - 1] = '\0';
+
+  return str;
 }
