e53ad6e89c8cdb43e5350c4c24690b0ea723a1b9
zahiiir <simo222zahir@gmail.com>
2023-10-31 11:52:10 -0700

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,79 +1,43 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
-		
- * argstostr - main entry
-		
- * @ac: int input
-		
- * @av: double pointer array
-		
- * Return: 0
-		
- */
-		
+* argstostr - concatène tous les arguments passés en paramètre
+* @ac: nombre d'arguments
+* @av: tableau de pointeurs vers les arguments
+* Return: pointeur vers la nouvelle chaîne de caractères concaténée
+*/
 char *argstostr(int ac, char **av)
-		
 {
-		
-	int i, n, r = 0, l = 0;
-		
-	char *str;
-		
+    int i, n, r = 0, l = 0;
+    char *str;
 
-		
-	if (ac == 0 || av == NULL)
-		
-		return (NULL);
-		
+    if (ac == 0 || av == NULL)
+        return (NULL);
 
-		
-	for (i = 0; i < ac; i++)
-		
-	{
-		
-		for (n = 0; av[i][n]; n++)
-		
-			l++;
-		
-	}
-		
-	l += ac;
-		
+    for (i = 0; i < ac; i++)
+    {
+        for (n = 0; av[i][n]; n++)
+            l++;
+        l++;
+    }
 
-		
-	str = malloc(sizeof(char) * l + 1);
-		
-	if (str == NULL)
-		
-		return (NULL);
-		
-	for (i = 0; i < ac; i++)
-		
-	{
-		
-	for (n = 0; av[i][n]; n++)
-		
-	{
-		
-		str[r] = av[i][n];
-		
-		r++;
-		
-	}
-		
-	if (str[r] == '\0')
-		
-	{
-		
-		str[r++] = '\n';
-		
-	}
-		
-	}
-		
-	return (str);
-		
+    str = malloc(sizeof(char) * (l + 1));
+
+    if (str == NULL)
+        return (NULL);
+
+    for (i = 0; i < ac; i++)
+    {
+        for (n = 0; av[i][n]; n++)
+        {
+            str[r] = av[i][n];
+            r++;
+        }
+        str[r] = '\n'; // Ajoute le caractère de nouvelle ligne à la position r
+        r++;
+    }
+
+    str[r] = '\0'; // Assure que la chaîne est correctement terminée par le caractère nul
+    return (str);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,83 +1,44 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
-		
- * str_concat - get ends of input and add together for size
-		
- * @s1: input one to concat
-		
- * @s2: input two to concat
-		
- * Return: concat of s1 and s2
-		
- */
-		
+* str_concat - concatène deux chaînes de caractères
+* @s1: première chaîne
+* @s2: deuxième chaîne
+* Return: pointeur vers la nouvelle chaîne de caractères concaténée
+*/
 char *str_concat(char *s1, char *s2)
-		
 {
-		
-	char *conct;
-		
-	int i, ci;
-		
+    char *conct;
+    int i, ci;
 
-		
-	if (s1 == NULL)
-		
-		s1 = "";
-		
-	if (s2 == NULL)
-		
-		s2 = "";
-		
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
 
-		
-		i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-		i++;
-		
-	while (s2[ci] != '\0')
-		
-		ci++;
-		
-	conct = malloc(sizeof(char) * (i + ci + 1));
-		
+    i = ci = 0;
+    while (s1[i] != '\0')
+        i++;
+    while (s2[ci] != '\0')
+        ci++;
 
-		
-	if (conct == NULL)
-		
-		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
-	}
-		
+    conct = malloc(sizeof(char) * (i + ci + 1));
+    if (conct == NULL)
+        return (NULL);
 
-		
-	while (s2[ci] != '\0')
-		
-	{
-		
-		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
-	}
-		
-	conct[i] = '\0';
-		
-	return (conct);
-		
+    i = ci = 0;
+    while (s1[i] != '\0')
+    {
+        conct[i] = s1[i];
+        i++;
+    }
+    while (s2[ci] != '\0')
+    {
+        conct[i] = s2[ci];
+        i++;
+        ci++;
+    }
+    conct[i] = '\0';
+    return (conct);
 }
